586a343e62484920043648325c132f15792db6a5
Chris <christophermyoder@gmail.com>
2019-03-24 15:19:31 +0000

    a commit I should have done yesterday

---
+++ unit15_DOM_Manipulation/notes/notes.js
@@ -0,0 +1,26 @@
+// var h1 = document.querySelector('h1');
+// h1.style.color = 'pink';
+
+
+// var body = document.querySelector('body');
+// var isBlue = false;
+
+// setInterval(function(){
+//     if(isBlue){
+//         body.style.background = 'white';
+//     }else{
+//         body.style.background = '#3486db';
+//     }
+//     isBlue = !isBlue;
+// }, 1000);
+
+// Imporant Selector Methods
+// document.getElementByID();
+    var tag = document.getElementById('highlight');
+    tag.style.backgroundColor = 'yellow';
+// document.getElementsByClassName();
+    var tags = document.getElementsByClassName('bolded');
+    tags.style
+// document.getElementsByTagName();
+// document.querySelector();
+// document.querySelectorAll();
