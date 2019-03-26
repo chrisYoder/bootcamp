1394d7d71219bdba31e8fd4d5188c4434ec86206
chrisYoder <christophermyoder@gmail.com>
2019-03-26 20:04:48 +0000

    eod

---
+++ unit16_Advanced_DOM_Manipulation/notes/notes.js
@@ -0,0 +1,3 @@
+var button = document.querySelector('button');
+var output = document.querySelector('#output');
+button.addEventListener('click', () => output.innerHTML = 'SOMEONE CLICKED THE BUTTON');
