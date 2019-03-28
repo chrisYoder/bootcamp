ce6f229af041f125f76994144f011163ae3ee437
chrisYoder <christophermyoder@gmail.com>
2019-03-28 20:25:57 +0000

    eod

--- unit17_Color_Game_Project/app.js
+++ unit17_Color_Game_Project/app.js
@@ -7,15 +7,13 @@ squares.forEach( square => {
 });
 
 
-
+// Random color generation
 function colorGenerator(){
   let red = getRandomInt(); 
   let blue = getRandomInt();
   let green = getRandomInt();
    
-   return `rgb(${red}, ${blue}, ${green})`;
-  
- 
+  return `rgb(${red}, ${blue}, ${green})`;
 }
 
 
@@ -24,4 +22,14 @@ function getRandomInt(){
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-console.log(colorGenerator());
+// I need to pick a random square to get the correct answer
+const correctColor = () => Math.floor(Math.random() * Math.floor(squares.length));
+const colorToGuess = () => {
+  let colorField = document.querySelector('#color');
+  let color = document.getElementById(correctColor());
+  let background = color.getAttribute('style')
+  
+  // background returns a string need to isolate the rgb part and then plug it into the html
+  
+}
+console.log(correctColor());
