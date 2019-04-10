0c2c5918915aa8b182556ede86549564a76e520c
Chris <christophermyoder@gmail.com>
2019-04-10 17:34:54 +0000

    last housekeeping commit

---
+++ unit25_ServerSideFrameWorks/examples/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// '/' => hi there
+
+app.get('/', (req, res) => res.send('Hi, there!'));
+
+// '/bye' => Goodbye!
+app.get('/bye', (req, res) =>res.send('Goodbye!'));
+
+// '/dog' => meow
+app.get('/dog', (req, res) => res.send('Meow!'));
+
+//route parameters
+app.get('/r/:subredditName/', (req, res) => {
+    let subReddit = req.params.subredditName;
+    res.send(`Welcom to the ${subReddit.toUpperCase()} Sub-Reddit`);
+});
+
+//catch all--returns this if there is no matching route. ie /cat returns this. should come last
+app.get('*', (req, res) => res.send('YOU ARE A STAR!'));
+
+// tell express to listen for requests(start server);
+app.listen(3000, process.env.IP, () => console.log('Server has started'));
